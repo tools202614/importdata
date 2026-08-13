f4d6d5e7040b6b65a7fe58c7fcde0dd34bd4cf9b
tools202614 <65067862+tools202614@users.noreply.github.com>
2026-08-13 21:26:39 +0800

    Update properties.ts

--- lib/properties.ts
+++ lib/properties.ts
@@ -4,6 +4,7 @@ export const PROPERTIES = [
   { id: "68dc7ebba02a4e194f76c998", name: "Bring The Stream" },
   { id: "65a7392f8d261e1b5f542d1b", name: "BringTheStreams" },
   { id: "67f9ab908cd1e51912b0721e", name: "Buckstreams" },
+  { id: "6a7a7d7445989d1d4159b07a", name: "Carefreestreams" },
   { id: "68e097a3243dae1952ff0b38", name: "Creativetain" },
   { id: "602d7e379c4f165d47c45c6d", name: "Dipiptv" },
   { id: "676eac1749e2fd8dfeff00f6", name: "Enterprise One" },
